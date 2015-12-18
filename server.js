bae19d54d6a1e72ec94b74620b38a7c8df323af1
leptone <leptone55@gmail.com>
2015-12-17 21:35:18 -0800

    changed port to dynamic process.env.port || 3000

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 console.log('Listening on port 3000 test')
 	
 // Routes
